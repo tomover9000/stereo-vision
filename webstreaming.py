d3f14ade2ee361e9378880c272b544e11da03472
tomover9000 <vlad.zorelscu@gmail.com>
2022-05-24 09:48:21 +0300

    added second camera

--- webstreaming.py
+++ webstreaming.py
@@ -5,18 +5,26 @@ import cv2
 
 #Initialize the Flask app
 app = Flask(__name__)
-camera = CameraStream(0)
+camera1 = CameraStream(0)
+camera2 = CameraStream(3)
 
 @app.route("/")
 def index():
 	# return the rendered template
 	return render_template("index.html")
 
-@app.route("/video_feed")
+@app.route("/video_feed1")
 def video_feed():
 	# return the response generated along with the specific media
 	# type (mime type)
-	return Response(camera.gen_frames(),
+	return Response(camera1.gen_frames(),
+		mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+@app.route("/video_feed2")
+def video_feed():
+	# return the response generated along with the specific media
+	# type (mime type)
+	return Response(camera2.gen_frames(),
 		mimetype = "multipart/x-mixed-replace; boundary=frame")
     
 
