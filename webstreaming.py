764dcecb27ab8c3c412b1adf7ed1306f55f0cd76
tomover9000 <vlad.zorelscu@gmail.com>
2022-05-24 09:50:15 +0300

    fixed video feeds

--- webstreaming.py
+++ webstreaming.py
@@ -14,14 +14,14 @@ def index():
 	return render_template("index.html")
 
 @app.route("/video_feed1")
-def video_feed():
+def video_feed1():
 	# return the response generated along with the specific media
 	# type (mime type)
 	return Response(camera1.gen_frames(),
 		mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 @app.route("/video_feed2")
-def video_feed():
+def video_feed2():
 	# return the response generated along with the specific media
 	# type (mime type)
 	return Response(camera2.gen_frames(),
